b6885fda14143faa8196bcb8fb73c1b410f9c6e0
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2025-06-25 09:11:50 +0200

    ascii screen wip

--- angular-version/src/editor.js
+++ angular-version/src/editor.js
@@ -92,6 +92,12 @@ const editor = (el, highlight = asm, tab = '    ') => {
 const el = document.querySelector('.editor');
 const lines = `
 MOV 925, 1
+.start:
+    MOV C, 1000
+.loop:
+    MOV [C], 1
+    ADD C, 2
+    JMP .loop
 `.split("\n");
 
 // clear existing content
--- angular-version/src/emulator/screen.js
+++ angular-version/src/emulator/screen.js
@@ -1,4 +1,4 @@
-app.service('screen', function () {
+app.service('screen', function ($document, cpu) {
     this.width = 32;
     this.height = 32;
     this.size = this.width * this.height * 2;
@@ -9,13 +9,24 @@ app.service('screen', function () {
         for (let y = 0; y < this.height; y++) {
             const row = [];
             for (let x = 0; x < this.width; x++) {
-                const addr = scope.displayStartIndex + (y * this.width + x)*2;
-                const value = scope.memory.load16(addr);
+            const addr = scope.displayStartIndex + (y * this.width + x) * 2;
+            const value = scope.memory.load16(addr);
+            if (cpu.screenMode) {
                 row.push(value & 1); // only black and white ATM.
-
+            } else {
+                if (String.fromCharCode(value) === '\0') {
+                    row.push(0); // Null character
+                } else {
+                    row.push(String.fromCharCode(value));
+                }
+            }
             }
             pixels.push(row);
         }
+        console.log("White", pixels[0] === 0);
+        console.log("Black", pixels[0] === 1);
+        console.log("ASCII", pixels[0] !== 0 && pixels[0] !== 1);
+        console.log("Screen pixels:", pixels);
         return pixels;
     };
 });
